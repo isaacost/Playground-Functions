76a37c829f832a6242f20689b26de3a75b212cb4
Isa Costa <isa-hcosta@hotmail.com>
2022-06-27 16:12:00 -0300

    requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -48,8 +48,17 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse);
+
+  if (distanciaCat1 < distanciaCat2) {
+    return 'cat1'
+  } else if (distanciaCat2 < distanciaCat1) {
+    return 'cat2'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
